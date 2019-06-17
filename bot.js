ebd7df0f16f40a6f1efb1fda970ae9ca9a8391e8
II_Snax_II <45282148+mohamkhalil9@users.noreply.github.com>
2019-06-17 20:26:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,62 +1,29 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+client.on('guildMemberAdd', async member => {
+	const channel = member.guild.channels.find(ch => ch.name === 'member-log');
+	if (!channel) return;
 
+	const canvas = Canvas.createCanvas(700, 250);
+	const ctx = canvas.getContext('2d');
 
+	// Since the image takes time to load, you should await it
+	const background = await Canvas.loadImage('./wallpaper.jpg');
+	// This uses the canvas dimensions to stretch the image onto the entire canvas
+	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+	// Use helpful Attachment class structure to process the file for you
+	const attachment = new Discord.Attachment(canvas.toBuffer(), 'welcome-image.png');
 
-
-
-client.on('guildMemberAdd', member => {
-    let channel = member.guild.channels.find('name', 'الترحيب🙌-•');
-    let memberavatar = member.user.avatarURL
-      if (!channel) return;
-    let embed = new Discord.RichEmbed()
-        .setColor('RANDOM')
-        .setThumbnail(memberavatar)
-        .addField(':running_shirt_with_sash: | name :  ',`${member}`)
-        .addField(':loudspeaker: | اطلق من دخل شخص جديد' , `اهلاً بك في السيرفر, ${member}`)
-        .addField('**id for user**', "**[" + `${member.id}` + "]**" )
-                .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
-               
-                  .addField("Name:",`<@` + `${member.id}` + `>`, true)
-                     
-                                     .addField(' الـسيرفر', `${member.guild.name}`,true)
-                                       
-     .setFooter(`${member.guild.name}`)
-        .setTimestamp()
-   
-      channel.sendEmbed(embed);
-    });
-    
-    client.on('guildMemberRemove', member => {
-        var embed = new Discord.RichEmbed()
-        .setAuthor(member.user.username, member.user.avatarURL)
-        .setThumbnail(member.user.avatarURL)
-        .setTitle(`لقد خرج صديق :( :raised_hand::skin-tone-1: :pensive:`)
-        .setDescription(`مع السلامة يا غالي :raised_hand::skin-tone-1: :pensive: `)
-        .addField(':bust_in_silhouette:   تبقي',`**[ ${member.guild.memberCount} ]**`,true)
-        .setColor('RED')
-        .setFooter(`====شكرا للاستمتاع====`, ' https://cdn.discordapp.com/attachments/397818254439219217/399292026782351381/shy.png')
-    
-    var channel =member.guild.channels.find('name', 'الترحيب🙌-•')
-    if (!channel) return;
-    channel.send({embed : embed});
-    })
-	
-	
-	client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : EX Clan`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Welcome | #♛ ExPlosive ♛`,"http://twitch.tv/Death Shop")
-client.user.setStatus("online")
+	channel.send(`Welcome to the server, ${member}!`, attachment);
 });
 
-	
-	
-	
-	
-	
-client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
+client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[member.guild.id];
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const xkiller = member.guild.channels.find("name", "welcome");
+     xkiller.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
+  });
+});
+
+
+client.login(process.env.BOT_TOKEN);
