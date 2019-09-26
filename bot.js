15755f44d2cee2351ed562f21ceffeb2ce23969f
II_Snax_II <45282148+mohamkhalil9@users.noreply.github.com>
2019-09-26 04:45:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -63,6 +63,8 @@ client.on('message', message => {
 client.on('message', message => {
     if (message.content === "تسجيل خروج") {
     let embed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setFooter(`Arab Force System`)
   .setColor("#000000")
   .addField(" :white_check_mark: تــــم تسجيل خروجك") // Alpha Codes Ghost
   
