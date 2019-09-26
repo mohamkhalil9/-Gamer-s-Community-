15cfb7d8c9ea57e911bb7754f477a8add870847d
II_Snax_II <45282148+mohamkhalil9@users.noreply.github.com>
2019-09-26 19:41:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "."
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -25,23 +25,7 @@ client.on('guildMemberAdd', member => {
 });
 
 
-if (command == "say") {
-  if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.sendMessage("**You Donot HavePermission ADMINISTRATOR**").then(m => m.delete(5000));
-    let say = new Discord.RichEmbed()
 
-    .setDescription(args.join(" "))
-
-    .setColor(0x23b2d6) // Alpha Codes Ghost
-
-    message.channel.sendEmbed(say);
-
-    message.delete();
-
-  }
-
-
-
-});
  
 
 
